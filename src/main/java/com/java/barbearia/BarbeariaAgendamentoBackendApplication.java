fba8ae6a528d20580f9824f9ff7d66921a2aa7d8
Bruno Mota <contatobrunotr@gmail.com>
2025-06-17 11:30:33 -0300

    feat: crorganização do projeto

---
+++ src/main/java/com/java/barbearia/BarbeariaAgendamentoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.java.barbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaAgendamentoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaAgendamentoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/barbearia/BarbeariaAgendamentoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.barbearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaAgendamentoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
